a06611e1e189fe10cc94b40c8cbc4b60957cf75b
SiarhKul <SiarhKul@gmail.com>
2020-10-31 22:56:04 +0300

    create request to server

--- src/components.huks/App.js
+++ src/components.huks/App.js
@@ -3,41 +3,35 @@ import Loader from './loader/Loader.js'
 import Button from './Button/Button.js'
 
 export default function App() {
-   // const [header, setheader] = useState({
-   //    name: ''
-   // })
+   const [header, setheader] = useState({
+      name: ''
+   })
 
    const [isLoaded, setIsLoaded] = useState({
       show: false
    });
 
-   // useEffect(() => {
-   //    setTimeout(() => {
-   //       setIsLoaded(true)
-   //    })
-
-   // }, [])
-
-   // const changeHeader = () => {
-   //    setTimeout(() => {
-   //       setheader({
-   //          name: 'hi Angular'
-   //       })
-   //    }, 3000);
-
-   // }
-
    const pushButton = () => {
       setIsLoaded({
          show: true
       })
+
+      setTimeout(() => {
+         setIsLoaded({
+            show: false
+         })
+
+         setheader({
+            name: 'hi Angular'
+         })
+      }, 3000);
       console.log(isLoaded);
    }
 
    return (
       <>
-         {/* <h1>{header.name}</h1> */}
-         <Loader />
+         <h1>{header.name}</h1>
+         {isLoaded.show ? <Loader /> : null}
          <Button click={pushButton}></Button>
       </>
    )
